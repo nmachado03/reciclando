10fc35ccf87c1828c94dafbf834d435087ed7c3b
nmachado03 <nykytoo36@gmail.com>
2020-02-24 13:38:25 -0300

    add geo.html

---
+++ src/static/js/geo.js
@@ -0,0 +1,34 @@
+function initMap() {
+    let coords = { lat: -34.776846, lng: -56.052895 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: coords,
+        zoom: 14
+    });
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        })
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
